80fed87aa32f1709fe18769ea3f6556d82b81914
WaltaZ <bartoszgdna@gmail.com>
2023-03-04 15:15:19 +0100

    lab1

---
+++ numerical methods/lab1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+double func(double);
+
+int main() {
+    double xl, xr, accuracy;
+
+    cout << "Specify x_l: ";
+    cin >> xl;
+    cout << "Specify x_r: ";
+    cin >> xr;
+    cout << "Specify accuracy (amount of digits): ";
+    cin >> accuracy;
+
+    if(func(xl) * func(xr) > 0){
+        cout << "Given range contains no root or more than one";
+        return 1;
+    } else if(xr <= xl) {
+        cout << "xl is bigger than xr";
+        return 1;
+    }
+
+    double tolerance = pow(0.1, accuracy),
+    error,
+    xm;
+    
+    int iterCounter;
+
+    do {
+        iterCounter++;
+
+        xm = (xl + xr)/2;
+        error = fabs((xr - xl) / xm);
+
+        if(func(xl) * func(xm) < 0){
+            xr = xm;
+        } else {
+            xl = xm;
+        }
+
+    } while(error > tolerance);
+
+    cout << "Found root x = " << setprecision(accuracy) << xm << " in " << iterCounter << " iterations";
+}
+
+double func(double x){
+    return cos(x) - x;
+}
